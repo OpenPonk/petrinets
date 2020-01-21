b892d4c51ff34612897e600fc54f2923ca811a3d
Jan Bliznicenko <jan.bliznicenko@fit.cvut.cz>
2020-01-21 17:20:19 +0100

    bundle vm to deploy

--- scripts/old/ssh.sh
+++
@@ -1,4 +0,0 @@
-openssl aes-256-cbc -K $encrypted_66bacb1c7d24_key -iv $encrypted_66bacb1c7d24_iv -in .ssh/id_petrinets_travis.enc -out .ssh/id_petrinets_travis -d
-chmod 0400 .ssh/id_petrinets_travis
-eval $(ssh-agent -s)
-ssh-add .ssh/id_petrinets_travis
