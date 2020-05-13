9f35ad3abe2f9081a84130579956b0c35e755c57
Jan Bliznicenko <jan.bliznicenko@fit.cvut.cz>
2020-05-13 15:26:17 +0200

    version storing attempt 5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -113,9 +113,9 @@ prepare_version_info() {
 
 	download_vm "$platform-threaded" $vm_dir
 
-	local version_info="{\"version\":\"${BUILD_VERSION}\",\"build_number\":${TRAVIS_BUILD_NUMBER},\"build_date\":\"${BUILD_TIMESTAMP}\",\"project_name\":\"${PROJECT_NAME}\"}"
+	local version_info="{\"version\":\"${BUILD_VERSION}\",\"build_number\":${TRAVIS_BUILD_NUMBER},\"build_timestamp\":\"${BUILD_TIMESTAMP}\",\"project_name\":\"${PROJECT_NAME}\"}"
 	echo "${version_info}" > version-info.json
-	"${vm_dir}"/pharo --encoding utf8 -vm-display-null -vm-sound-null $SMALLTALK_CI_IMAGE eval --save "NeoJSONReader fromString: '${version_info}'. OPVersion currentFromJSON: '${version_info}'"
+	"${vm_dir}"/pharo --encoding utf8 -vm-display-null -vm-sound-null $SMALLTALK_CI_IMAGE eval --save "Transcript show: (NeoJSONReader fromString: '${version_info}') asString. OPVersion currentFromJSON: '${version_info}'"
 }
 
 upload_version_info() {
