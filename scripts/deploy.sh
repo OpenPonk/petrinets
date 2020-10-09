8f4d444e77f1b677a85f1df77af460ca2f898fd5
Jan Blizničenko <9571633+JanBliznicenko@users.noreply.github.com>
2020-10-09 22:20:35 +0200

    Update travis config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,7 +35,7 @@ upload() {
 	local platform=$1
 	local package_dir_name="openponk-$PROJECT_NAME-$platform"
 	local working_dir="$package_dir_name-$BUILD_VERSION"
-	local zip="$package_dir_name-latest.zip"
+	local zip="$package_dir_name-$BUILD_VERSION.zip"
 
 	cd "$working_dir"
 	zip -qr "$zip" "${package_dir_name}"
