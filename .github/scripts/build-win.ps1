d35fae18fb099d869bb7c64a72af0fc3851d1e62
Jan Bliznicenko <jan.bliznicenko@fit.cvut.cz>
2021-10-05 09:37:29 +0200

    CTU network drive issue workaround

--- .github/scripts/build-win.ps1
+++ .github/scripts/build-win.ps1
@@ -22,7 +22,11 @@ cp $vm_dir/*.dll $package_dir/Pharo
 cp $vm_dir/Pharo.exe $package_dir/Pharo
 cp $vm_dir/PharoConsole.exe $package_dir/Pharo
 
-"start %~dp0Pharo\Pharo.exe %~dp0image\$PROJECT_NAME.image" | set-content "$package_dir/$PROJECT_NAME.bat"
+"`$openponk_path=`"`$PSScriptRoot\`" -replace '\\\\','\' -replace '\\+.*\.cvut\.cz\\[^\\]*\\[^\\]*','X:'
+echo `"Opening OpenPonk on path: `${openponk_path}`"
+Start-Process -FilePath `${openponk_path}Pharo\Pharo.exe `${openponk_path}image\$PROJECT_NAME.image" | set-content "$package_dir/$PROJECT_NAME.ps1"
+
+"powershell -File %~dp0$PROJECT_NAME.ps1" | set-content "$package_dir/$PROJECT_NAME.bat"
 
 & $vm_dir/PharoConsole.exe -headless $package_dir/image/$PROJECT_NAME.image eval --save "OPVersion currentWithRunId: $RUN_ID projectName: '$REPOSITORY_NAME'"
 
